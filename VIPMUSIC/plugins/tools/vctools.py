484be207372321f507150a4a125accc2b556e156
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2024-02-27 15:56:15 +0530

    Update vctools.py

--- VIPMUSIC/plugins/tools/vctools.py
+++ VIPMUSIC/plugins/tools/vctools.py
@@ -12,7 +12,7 @@ async def brah(_, msg):
 # vc off
 @app.on_message(filters.video_chat_ended)
 async def brah2(_, msg):
-    await msg.reply("**😕ᴠɪᴅᴇᴏ ᴄʜᴀᴛ ᴇɴᴅᴇᴅ💔**")
+    await msg.reply("**😕GADARI KARBE💔**")
 
 # invite members on vc
 @app.on_message(filters.video_chat_members_invited)
